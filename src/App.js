dacf22e8611dc3cf118445e7aa4591c42cef5eef
maranata122 <maranathameseret@gmail.com>
2026-01-30 11:08:59 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from './data';
+import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
 
 function App() {
+  const [cart, setCart] = useState([]);
+  // function addToCart(book) {
+  //   //when we use hook in an array
+  //   //we need to pass it in to
+  //   //a new array
+  //   const dupeItem = cart.find((item) => +item.id === +book.id);
+  //   if (dupeItem) {
+  //     setCart(
+  //       cart.map((item) => {
+  //         if (+item.id === +dupeItem.id) {
+  //           return {
+  //             ...item,
+  //             quantity: item.quantity + 1,
+  //           };
+  //         } else {
+  //           return item;
+  //         }
+  //       })
+  //     );
+  //   } else {
+  //     setCart([...cart, { ...book, quantity: 1 }]);
+  //   }
+  //   //... spread operator
+  // }
+  // useEffect(() => {
+  //   console.log(cart);
+  // }, [cart]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+  function removeItem(item) {
+    setCart(cart.filter((book) => book.id !== item.id));
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  useEffect(() => {}, [cart]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()}></Nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} addToCart={addToCart} cart={cart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          )}
+        />
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Explore.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Explore() {
+  return (
+    <section id="explore">
+      <div className="row row__column">
+        <h2>
+          Explore More <span className="purple">Books</span>
+        </h2>
+        <Link to="/books">
+          <button className="btn">Explore Books</button>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default Explore;
